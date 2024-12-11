0a942cf0c291f570ce41d3ce2d83609b1ff3f5c5
Yozi Reci Manda <YRManda@maybank.co.id>
2024-12-12 04:07:44 +0700

    Initial commit

---
+++ IOSJenkinsFastlane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSJenkinsFastlane
+//
+//  Created by Yozi Reci Manda on 12/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Running Apps.")
+    }
+
+
+}
+
